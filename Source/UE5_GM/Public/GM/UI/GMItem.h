9d866dc07694d8e72eb8f112848a0d34a1046c88
Nanfengzhiwo1 <158132087@qq.com>
2025-10-16 16:18:57 +0800

    Designing the UI for Tabs and Items

---
+++ Source/UE5_GM/Public/GM/UI/GMItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GMItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GM_API UGMItem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
